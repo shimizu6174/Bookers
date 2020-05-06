884a712bc8e35fb76eb305142143f67dc48f1d71
ShimiKo <nyol76@icloud.com>
2020-05-06 09:48:48 +0900

    投稿機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,23 @@
 class BooksController < ApplicationController
   def index
+  	@book = Book.new
+  	@books = Book.all
   end
 
   def show
   end
 
-  def new
-  end
-
   def edit
   end
+
+  def create
+        book = Book.new(book_params)
+        book.save
+        redirect_to root_path
+    end
+
+    private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 end
