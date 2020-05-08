d0e79be913869d05123d97317a73ffe4f8b394ea
ShimiKo <nyol76@icloud.com>
2020-05-08 13:12:34 +0900

    code finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,35 +1,49 @@
 class BooksController < ApplicationController
 
-  def top
-  end
+	def top
+	end
 
-  def index
-  	@book = Book.new
-  	@books = Book.all
-  end
+	def index
+		@book = Book.new
+		@books = Book.all
+	end
 
-  def show
-  	@book = Book.find(params[:id])
-  end
+	def show
+		@book = Book.find(params[:id])
+	end
 
-  def edit
-  	@book = Book.find(params[:id])
-  end
+	def edit
+		@book = Book.find(params[:id])
+	end
 
-  def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to book_path(book)
-  end
+	def update
+		book = Book.find(params[:id])
+		if book.update(book_params)
+			flash[:notice] = "Book was successfully updated."
+			redirect_to book_path(book)
+		else
+			redirect_to edit_book_path, flash: { error: book.errors.full_messages }
+		end
+	end
 
-  def create
-        book = Book.new(book_params)
-        book.save
-        redirect_to books_path
-    end
+	def destroy
+		book = Book.find(params[:id])
+		book.destroy
+		redirect_to books_path
+	end
 
-    private
-    def book_params
-      params.require(:book).permit(:title, :body)
-    end
+	def create
+		book = Book.new(book_params)
+		if book.save
+			flash[:notice] = "Book was successfully created."
+			redirect_to book_path(book)
+		else
+			redirect_to books_path, flash: { error: book.errors.full_messages }
+		end
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
+	end
 end
